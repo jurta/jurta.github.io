676d7fb7d68072339c61e4d06274ac1fd712c35e
Juri Linkov <juri@jurta.org>
2020-05-07 01:53:17 +0300

    Add images from 2003

---
+++ files/prog/jekyll/picasa_3.sh
@@ -0,0 +1,71 @@
+# mkdir -p /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989
+# cp --no-preserve=mode /mnt/home/work/www/backup/jurta.org.ua/tanja/1989/01_0150.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/01_0150.jpg
+# exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Мы с Таней на дне рождения в 1989 году" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/01_0150.jpg
+# cp --no-preserve=mode /mnt/home/work/www/backup/jurta.org.ua/tanja/1989/02_0150.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/02_0150.jpg
+# exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Мы с Таней на дне рождения в 1989 году" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/02_0150.jpg
+# cp --no-preserve=mode /mnt/home/work/www/backup/jurta.org.ua/tanja/1989/03_0150.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/03_0150.jpg
+# exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Мы с Таней на дне рождения в 1989 году" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/03_0150.jpg
+# cp --no-preserve=mode /mnt/home/work/www/backup/jurta.org.ua/tanja/1989/04_0150.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/04_0150.jpg
+# exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Мы с Таней на дне рождения в 1989 году" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/04_0150.jpg
+# cp --no-preserve=mode /mnt/home/work/www/backup/jurta.org.ua/tanja/1989/05_0150.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/05_0150.jpg
+# exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Мы с Таней на дне рождения в 1989 году" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/05_0150.jpg
+# cp --no-preserve=mode /mnt/home/work/www/backup/jurta.org.ua/tanja/1989/06_0150.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/06_0150.jpg
+# exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Мы с Таней на дне рождения в 1989 году" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/06_0150.jpg
+# cp --no-preserve=mode /mnt/home/work/www/backup/jurta.org.ua/tanja/1989/07_0150.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/07_0150.jpg
+# exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Мы с Таней на дне рождения в 1989 году" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/1989/07_0150.jpg
+mkdir -p /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003
+# SEE INFO at /home/juri/work/media/image/foto/foto.eet
+# SEE INFO at /home/juri/work/media/image/foto/README
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20020929_0007.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20020929_0008.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20020929_0023.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20020929_0024.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20020929_0025.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030314_0018.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030314_0019.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030314_0020.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030314_0021.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030314_0022.jpg
+convert -resize 33% /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030317_0001.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030317_0001.jpg
+exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Таня идёт на работу, Ново-Дарницкая 27" -GPSLatitude="50.428145" -GPSLatitudeRef="North" -GPSLongitude="30.662577" -GPSLongitudeRef="East" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030317_0001.jpg
+convert -resize 33% /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030317_0002.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030317_0002.jpg
+exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Таня идёт на работу, Ново-Дарницкая 27" -GPSLatitude="50.428145" -GPSLatitudeRef="North" -GPSLongitude="30.662577" -GPSLongitudeRef="East" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030317_0002.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030317_0016.jpg
+convert -resize 33% /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030317_0022.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030317_0022.jpg
+exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Таня с детьми идёт в д/с Познайка" -GPSLatitude="50.527839" -GPSLatitudeRef="North" -GPSLongitude="30.603496" -GPSLongitudeRef="East" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030317_0022.jpg
+convert -resize 33% /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030317_0023.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030317_0023.jpg
+exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Таня с детьми идёт в д/с Познайка" -GPSLatitude="50.527839" -GPSLatitudeRef="North" -GPSLongitude="30.603496" -GPSLongitudeRef="East" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030317_0023.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030317_0024.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030317_0027.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030715_0014.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030715_0015.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030723_0006.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030723_0007.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030723_0008.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030723_0009.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030729_0002.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030729_0003.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030729_0018.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030729_0021.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030729_0022.jpg
+# TODO:
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0001.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0002.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0003.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0004.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0005.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0006.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0007.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0008.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0009.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0010.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0011.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0012.jpg
+convert -resize 33% /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0014.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030801_0014.jpg
+exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Таня и Валя с Мамой" -GPSLatitude="59.436116" -GPSLatitudeRef="North" -GPSLongitude="24.822080" -GPSLongitudeRef="East" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030801_0014.jpg
+convert -resize 33% /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0015.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030801_0015.jpg
+exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Таня и Валя с Мамой" -GPSLatitude="59.436116" -GPSLatitudeRef="North" -GPSLongitude="24.822080" -GPSLongitudeRef="East" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030801_0015.jpg
+convert -resize 33% /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0016.jpg /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030801_0016.jpg
+exiftool -overwrite_original_in_place -PreviewImage= -ThumbnailImage= -Artist="Juri Linkov" -Copyright="(c) juri@jurta.org" -XMP-cc:License="http://creativecommons.org/licenses/by-sa/4.0/" -ImageDescription="Таня и Валя с Мамой" -GPSLatitude="59.432068" -GPSLatitudeRef="North" -GPSLongitude="24.822075" -GPSLongitudeRef="East" /home/work/www/jurta.org/jekyll/jurta.github.io/WORK/2003/20030801_0016.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0017.jpg
+# /mnt/home/work/www/backup/jurta.org.ua/tanja/2003/20030801_0018.jpg
